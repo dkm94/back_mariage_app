3efe60a8618b553e055641f20d799b9b38eb4cef
dkm94 <diane.mpacko@gmail.com>
2021-01-21 02:45:26 +0100

    trying fixing headers options

--- server.js
+++ server.js
@@ -20,6 +20,25 @@ const corsOptions = {
     credentials: true
   }
 app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    // res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://my-wedding-app.netlify.app/");
+    // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
+  
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+  
+    // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  
+    // Pass to next layer of middleware
+    next();
+  });
 app.use(bearerToken());
 
 // BDD connection
