27550954db254e076fb28d2b487eafae2f11cc21
dkm94 <diane.mpacko@gmail.com>
2021-09-02 20:46:59 +0200

    added date to expense obj

--- src/models/budget_operation.js
+++ src/models/budget_operation.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 var Currency = mongoose.Types.Currency;
+const start = Date.now();
+const date = new Date(start);
+const formatted = date.toLocaleDateString("fr-FR");
 
 let operationSchema = new mongoose.Schema({
     
@@ -18,6 +21,10 @@ let operationSchema = new mongoose.Schema({
     price: { 
         type: Number
     },
+    date: {
+        type: String,
+        default: formatted
+    }
     budgetID: {
         type: Schema.Types.ObjectId, 
         ref: 'Budget'
