38f2a63641b8be31cb513e572b0b530d97b87103
Diane MPACKO <diane@MacBook-Air-de-Diane.local>
2022-08-28 20:57:30 +0200

    update staters controller

--- src/controllers/starter_controller.js
+++ src/controllers/starter_controller.js
@@ -1,22 +1,14 @@
-const Menu = require('../models/menu');
 const Starter = require('../models/menu-entrée');
 
-
-// //ENTREE
 exports.newStarter = (req, res) => {
     const mariageId = res.locals.mariageID;
     let starter = new Starter ({
         ...req.body,
-        menuID: req.params.id,
         mariageID: mariageId
     });
     starter.save()
-        .then(newStarter => {
-            Menu.updateOne({_id: req.params.id, mariageID: mariageId},
-                {$push: {starterID: newStarter._id}})
-                .then(data => res.status(200).json(newStarter))
-        })
-        .catch(err => res.status(400).json({err}))
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(400).json( err ))
 }
 
 exports.starters = (req, res, next) => {
@@ -37,13 +29,6 @@ exports.updateStarter = (req, res, next) => {
 exports.deleteStarter = (req, res, next) => {
     const mariageId = res.locals.mariageID;
     Starter.deleteOne({_id: req.params.id, mariageID: mariageId})
-        .then(data => {
-        if(data.deletedCount == 1){
-            Menu.updateMany({mariageID: mariageId}, {$pull: {starterID: req.params.id}})
-                .then(data => res.status(200).json(data))
-                .catch(err => res.status(400).json(err))
-        } else
-            return res.status(400).json('erreur deleted count')
-        })
+        .then(data => res.status(200).json(data))
         .catch(err => res.status(400).json ({ err }))
 }
--- src/routes/starter_router.js
+++ src/routes/starter_router.js
@@ -8,7 +8,7 @@ const { adminAuth } = require("../middlewares");
 
 const router = express.Router();
 
-router.post("/add/:id", adminAuth, newStarter);
+router.post("/add", adminAuth, newStarter);
 router.get("/", adminAuth, starters);
 router.post("/edit/:id", adminAuth, updateStarter);
 router.delete("/delete/:id", adminAuth, deleteStarter);
