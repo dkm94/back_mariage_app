ff89d0383dd06e0d300e7fe00567e8c15206685e
Diane MPACKO <diane@MacBook-Air-de-Diane.local>
2022-08-28 21:04:19 +0200

    update maincourse controller and router

--- src/controllers/mainCourse_controller.js
+++ src/controllers/mainCourse_controller.js
@@ -1,20 +1,14 @@
-const Menu = require('../models/menu');
 const Maincourse = require("../models/menu-plat")
 
 exports.newMaincourse = (req, res) => {
     const mariageId = res.locals.mariageID;
     let maincourse = new Maincourse ({
         ...req.body,
-        menuID: req.params.id,
         mariageID: mariageId
     });
     maincourse.save()
-        .then(newMaincourse => {
-            Menu.updateOne({_id: req.params.id, mariageID: mariageId},
-                {$push: {maincourseID: newMaincourse._id}})
-                .then(data => res.status(200).json(newMaincourse))
-        })
-        .catch(err => res.status(400).json({err}))
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(400).json( err ))
 }
 
 exports.maincourses = (req, res, next) => {
@@ -35,13 +29,6 @@ exports.updateMaincourse = (req, res, next) => {
 exports.deleteMaincourse = (req, res, next) => {
     const mariageId = res.locals.mariageID;
     Maincourse.deleteOne({_id: req.params.id, mariageID: mariageId})
-        .then(data => {
-        if(data.deletedCount == 1){
-            Menu.updateMany({mariageID: mariageId}, {$pull: {maincourseID: req.params.id}})
-                .then(data => res.status(200).json(data))
-                .catch(err => res.status(400).json(err))
-        } else
-            return res.status(400).json('erreur deleted count')
-        })
+        .then(data => res.status(200).json(data))
         .catch(err => res.status(400).json ({ err }))
 }
