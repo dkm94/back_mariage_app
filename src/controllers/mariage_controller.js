80686a630470c1e0ec0c4ace5e6e5bfb9f3d8bd4
dkm94 <diane.mpacko@gmail.com>
2024-01-10 13:52:06 +0100

    updated getWedding

--- src/controllers/mariage_controller.js
+++ src/controllers/mariage_controller.js
@@ -1,10 +1,21 @@
 const Wedding = require("../models/mariage");
 
-exports.mariage = (req, res) => {
-    const mariageId = res.locals.mariageID;
-    Wedding.findOne({ _id: mariageId})
-        .then(data => res.status(200).json(data))
-        .catch(err => res.status(400).json({ err}))
+exports.mariage = async (req, res) => {
+    const { locals } = res;
+    const mariageID = locals.mariageID;
+
+    try {
+        const userWedding = await Wedding.findOne({ _id: mariageID })
+        
+        if(!userWedding){
+            res.send({ success: false, message: "Impossible de charger les données du mariage", statusCode: 404 })
+            return;
+        }
+
+        res.send({ success: true, data: userWedding, statusCode: 200 });
+    } catch (err) {
+        res.send({ success: false, message: "Echec serveur", statusCode: 500 })
+    }
 }
 
 exports.updateMariage = (req, res) => {
