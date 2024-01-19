ba1e580232119d3305be03976d0bee242fc424de
dkm94 <diane.mpacko@gmail.com>
2024-01-19 15:08:53 +0100

    + operation controller ok

--- src/controllers/budget_controller.js
+++ src/controllers/budget_controller.js
@@ -1,5 +1,10 @@
 const Operation = require("../models/budget_operation");
 
+const findOperationById = async (id) => {
+    const operation = await Operation.findById({ _id: id })
+    return operation;
+}   
+
 exports.operations = async (req, res) => {
     const { locals } = res;
     const mariageID = locals.mariageID;
@@ -18,38 +23,67 @@ exports.operations = async (req, res) => {
     }
 }
 
-exports.operation = (req, res) => {
-    Operation.findOne({ _id: req.params.id })
-        .then(data => {
-            res.status(200).json(data)
-            console.log(data.price.toFixed(2)/100)
-        })
-        .catch(err => res.status(400).json(err))
+exports.operation = async (req, res) => {
+    try {
+        const data = await Operation.findOne({ _id: req.params.id });
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(400).json({ success: false,  message: err.message || "Oups, une erreur s'est produite lors de la récupération de l'opération" });
+    }
 }
 
-exports.newOperation = (req, res) => {
-    const mariageId = res.locals.mariageID;
-    let operation = new Operation ({
-        ...req.body,
-        price: req.body.price * 100,
-        mariageID: mariageId
-    });
-    operation.save()
-        .then(data => res.status(200).json(data))
-        .catch(err => res.status(400).json (err))
+exports.newOperation = async (req, res) => {
+    try {
+        const mariageId = res.locals.mariageID;
+        let operation = new Operation({
+            ...req.body,
+            price: req.body.price * 100,
+            mariageID: mariageId
+        });
+
+        const data = await operation.save();
+        res.status(200).json({ success: true, data });
+    } catch (err) {
+        res.status(400).json({ success: false,  message: err.message || "Oups, une erreur s'est produite lors de la création de l'opération" });
+    }
 }
 
-exports.updateOperation = (req, res) => {
-    const mariageId = res.locals.mariageID;
-    Operation.updateOne({_id: req.params.id},
-        {...req.body, _id: req.params.id, mariageID: mariageId})
-    .then(data => res.status(200).json(data))
-    .catch(err => res.status(400).json (err))
+exports.updateOperation = async (req, res) => {
+    try {
+        const mariageId = res.locals.mariageID;
+        
+        const existingOperation = await findOperationById(req.params.id);
+        if (!existingOperation) {
+            return res.status(404).json({ success: false, message: "L'opération n'existe pas." });
+        }
+
+        const { category, description, price } = req.body;
+        const validCategories = [
+            'Locations', 'Habillement/Beauté', 'Décoration/Fleurs', 'Alliances/Bijoux', 'Animation', 'Traiteur', 'Faire-part', 'Autres'
+        ];
+
+        if (!category || !validCategories.includes(category) || !description || typeof price !== 'number') {
+            return res.status(400).json({ err: "Tous les champs doivent être valides." });
+        }
+
+        const result = await Operation.updateOne(
+            { _id: req.params.id },
+            { ...req.body, _id: req.params.id, mariageID: mariageId }
+        );
+
+        res.status(200).json({ success: true, message: result });
+    } catch (err) {
+        res.status(400).json({ success: false, message: err.message || "Oups, une erreur s'est produite lors de la mise à jour de l'opération." });
+    }
 }
 
-exports.deleteOperation = (req, res) => {
-    const mariageId = res.locals.mariageID;
-    Operation.deleteOne({ _id: req.params.id, mariageID: mariageId })
-        .then(data => res.status(200).json(data))
-        .catch(err => res.status(400).json(err))
+exports.deleteOperation = async (req, res) => {
+    try {
+        const mariageId = res.locals.mariageID;
+        const result = await Operation.deleteOne({ _id: req.params.id, mariageID: mariageId });
+
+        res.status(200).json({ success: true, data: result });
+    } catch (err) {
+        res.status(400).json({ success: false, message: err.message || "Oups, une erreur s'est produite lors de la suppression de l'opération" });
+    }
 }
